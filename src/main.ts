da0233e6afd1c8f7700c067fe7d4a3f1122043a0
Behzad Khalifeh <bkhalifeh@protonmail.com>
2024-11-23 02:47:31 +0330

    feat: setup pug template engine

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.setViewEngine('pug');
+  app.setBaseViewsDir(join(process.cwd(), 'views'));
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
