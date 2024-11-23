ba66d635a17261ef7de3ea9c37c71363718db9ca
Behzad Khalifeh <bkhalifeh@protonmail.com>
2024-11-23 15:12:12 +0330

    feat: setup logger and favicon

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+import favicon from 'serve-favicon';
+import morgan from 'morgan';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.setViewEngine('pug');
   app.setBaseViewsDir(join(process.cwd(), 'views'));
+
+  app.use(favicon(join(process.cwd(), 'static', 'img', 'favicon.ico')));
+  app.use(morgan('dev'));
 
   await app.listen(process.env.PORT ?? 3000);
 }
