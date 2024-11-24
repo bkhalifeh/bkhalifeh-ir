c4e164ae6fe842d32e00a6b134a50c9f7416257c
Behzad Khalifeh <bkhalifeh@protonmail.com>
2024-11-24 22:14:19 +0330

    feat: get port and host from user

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,11 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import favicon from 'serve-favicon';
 import morgan from 'morgan';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-
+  const configService = app.get(ConfigService);
   app.disable('x-powered-by');
   app.setViewEngine('pug');
   app.setBaseViewsDir(join(process.cwd(), 'views'));
@@ -15,6 +16,9 @@ async function bootstrap() {
   app.use(favicon(join(process.cwd(), 'static', 'img', 'favicon.ico')));
   app.use(morgan('dev'));
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(
+    configService.get<number>('APP_PORT', 3000),
+    configService.get<string>('APP_HOST', '0.0.0.0'),
+  );
 }
 bootstrap();
