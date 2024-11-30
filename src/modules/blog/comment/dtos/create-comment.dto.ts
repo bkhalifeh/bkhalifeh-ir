92f1e1bde2f33b071245d355efddafdaca155f78
Behzad Khalifeh <bkhalifeh@protonmail.com>
2024-11-30 05:40:09 +0330

    feat: add validation

--- src/modules/blog/comment/dtos/create-comment.dto.ts
+++ src/modules/blog/comment/dtos/create-comment.dto.ts
@@ -1,4 +1,15 @@
+import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
+
 export class CreateCommentDto {
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(256)
+  @MinLength(3)
   name: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(2048)
+  @MinLength(1)
   content: string;
 }
--- src/modules/contact/dtos/create-contact.dto.ts
+++ src/modules/contact/dtos/create-contact.dto.ts
@@ -1,5 +1,26 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
 export class CreateContactDto {
+  @IsNotEmpty()
+  @IsString()
+  @IsEmail()
   email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(256)
+  @MinLength(3)
   name: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(2048)
+  @MinLength(1)
   content: string;
 }
