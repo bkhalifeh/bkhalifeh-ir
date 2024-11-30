daa0a556ee99a9eda3ffd93105bc2d7a3d529001
Behzad Khalifeh <bkhalifeh@protonmail.com>
2024-11-30 06:13:40 +0330

    refactor: improve security

--- src/modules/blog/post/post.controller.ts
+++ src/modules/blog/post/post.controller.ts
@@ -1,6 +1,8 @@
-import { Controller, Get, Param, Render } from '@nestjs/common';
+import { Controller, Get, Param, Render, Res } from '@nestjs/common';
 import { PostService } from './post.service';
 import { InfoService } from '../../info/info.service';
+import slugify from 'slugify';
+import { Response } from 'express';
 
 @Controller('blog')
 export class PostController {
@@ -25,16 +27,19 @@ export class PostController {
   }
 
   @Get(':slug')
-  @Render('single-post')
-  async findOne(@Param('slug') slug: string) {
-    const info = await this.infoService.find();
-    const post = await this.postService.findOne(slug);
-    return {
-      author: info.name,
-      title: post.title,
-      description: post.summary,
-      info,
-      post,
-    };
+  async findOne(@Param('slug') slug: string, @Res() res: Response) {
+    const post = await this.postService.findOne(slugify(slug));
+    if (post) {
+      const info = await this.infoService.find();
+      res.render('single-post', {
+        author: info.name,
+        title: post.title,
+        description: post.summary,
+        info,
+        post,
+      });
+    } else {
+      res.redirect('/blog');
+    }
   }
 }
